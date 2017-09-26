9141867b39d62cb4ff5b7c49292115ef4f8e0781
callertrain <cahcardeal@gmail.com>
2017-09-26 00:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(){
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(){
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(){
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(){
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.splice();
+}
